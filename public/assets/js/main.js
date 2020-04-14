ff1b54e2d3a2b90c0fab06371e523e36cbabbbea
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-04-14 19:42:20 -0400

    cambios

---
+++ public/assets/js/main.js
@@ -0,0 +1,32 @@
+$(function () {
+  'use strict'
+
+  $('[data-toggle="offcanvas"]').on('click', function () {
+    $('.offcanvas-collapse').toggleClass('open')
+  })
+})
+
+$(document).ready(function() {
+	$(".megamenu").on("click", function(e) {
+		e.stopPropagation();
+	});
+});
+
+
+// Menú fixed
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 70 && ($(window).width() >= 768)) {
+   
+    // $('.navbar-fixed-js').addClass('top');
+    // $('.navbar-fixed-js').removeClasss('top');
+  } else {
+    // $('.navbar-fixed-js').removeClass('fixed');
+    // $('.nav-link').removeClass('fixed-color');
+    // $('.nav-top__header').removeClass('nav-top__header--detele');
+    // $("#iso").removeClass('img-size').attr('src', 'assets/img/logo.jpeg').removeClass('scroll-up');
+    // $('.navbar').addClass('top');
+ 
+    // $('.navbar-fixed-js').addClass('top');
+    // $('.navbar-fixed-js').removeClasss('top');
+  }
+});
