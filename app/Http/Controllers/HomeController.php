9af8e2aee5b8338924288c6dbcd5a5149f2289cf
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-04-20 12:08:30 -0400

    cambios

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Product;
 use App\Category;
 use App\Brand;
+use DB;
 
 class HomeController extends Controller
 {
@@ -28,14 +29,53 @@ class HomeController extends Controller
 
     function search(Request $request){
 
+        $words = explode(' ',$request->search);
+        $wordsToDelete = array('de');
 
-        $products = Product::with("category")->join('brands', 'products.brand_id', '=', 'brands.id')->where('products.name', 'like', '%'.$request->search.'%')->orWhere('products.sub_title', 'like', '%'.$request->search.'%')->orWhere('products.description', 'like', '%'.$request->search.'%')->orWhere("brands.name", 'like', '%'.$request->search.'%')->get();
+        $words = array_values(array_diff($words,$wordsToDelete));
+        
+        //DB::statement('ALTER TABLE products ADD FULLTEXT fulltext_index (name, description)');
+        //$products = Product::where('name', 'like', '%'.$request->search.'%')->get();
 
-        dd($products);
+        $products = Product::with('category')->join('categories', 'products.category_id', '=', 'categories.id')->join('brands', 'products.brand_id','=', 'brands.id')
+                    ->where(function ($query) use($words) {
+                        for ($i = 0; $i < count($words); $i++){
+                            $query->orWhere('products.name', $words[$i]);
+                        }      
+                    })
+                    ->orWhere(function ($query) use($words) {
+                        for ($i = 0; $i < count($words); $i++){
+                            $query->orWhere('categories.name', 'like',  '%' . $words[$i] .'%');
+                        }      
+                    })
+                    ->orWhere(function ($query) use($words) {
+                        for ($i = 0; $i < count($words); $i++){
+                        $query->orwhere('products.description', 'like', '%'.$words[$i].'%');
+                        }      
+                    })
+                    ->orWhere(function ($query) use($words) {
+                        for ($i = 0; $i < count($words); $i++){
+                        $query->orwhere('products.sub_title', $words[$i]);
+                        }      
+                    })
+                    ->orWhere(function ($query) use($words) {
+                        for ($i = 0; $i < count($words); $i++){
+                        $query->orwhere('brands.name', 'like',  '%' . $words[$i] .'%');
+                        }      
+                    })
+                    ->get();
 
-        $brands = Brand::where("name", "like", '%'.$request->search.'%')->get();
+        $brands = Brand::where(function ($query) use($words) {
+            for ($i = 0; $i < count($words); $i++){
+                $query->orWhere('brands.name', 'like',  '%' . $words[$i] .'%');
+            }      
+        })->get();
 
-        $categories = Category::where('name', 'like', '%'.$request->search.'%')->get();
+        $categories = Category::where(function ($query) use($words) {
+            for ($i = 0; $i < count($words); $i++){
+                $query->orWhere('categories.name', 'like',  '%' . $words[$i] .'%');
+            }      
+        })->get();
 
         return view('search', ["products" => $products, "brands" => $brands, "categories" => $categories]);
 
