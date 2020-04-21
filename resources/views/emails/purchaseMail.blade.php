e7a0a2827c30c0bd488f862e482d17921ddb62bc
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-04-20 20:57:32 -0400

    cambios

--- resources/views/emails/purchaseMail.blade.php
+++ resources/views/emails/purchaseMail.blade.php
@@ -1,7 +1,5 @@
 <h1>Ha realizado una compra</h1>
 
-
-
-<p>Hola {{ $user['name'] }}, sigue este enlace para cambiar tu clave <a href="{{ url('/password/recovery/restore/'.$user['recovery_hash']) }}">recuperar</a></p>
+<p>Hola {{ $user['name'] }}, has realizado una compra con exito </p>
 
 
