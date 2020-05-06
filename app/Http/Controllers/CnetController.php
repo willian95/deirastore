0064d2a5b6c557493d3b3a405481fb1824f9d004
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-05-05 23:01:17 -0400

    cambios

--- app/Http/Controllers/CnetController.php
+++ app/Http/Controllers/CnetController.php
@@ -15978,7 +15978,7 @@ class CnetController extends Controller
 
         ];
 
-        $products = new \LimitIterator(new \ArrayIterator($array), 3455, 1001);
+        $products = new \LimitIterator(new \ArrayIterator($array), 4455, 1001);
         foreach($products as $arr){
             $client = new \GuzzleHttp\Client();
             $request = $client->request("GET", 'http://ws.cnetcontent.com/a67a9f7e/api/72a5f0b5f9?cpn='.$arr.'&lang=es&market=cl');
