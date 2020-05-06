e456989518d8adc6f02b5ca0e9cc6d0e285d6b00
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-05-06 00:13:48 -0400

    cambios

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,7 +47,10 @@ return [
             'driver' => 'local',
             'root' => storage_path('app'),
         ],
-
+        'cnet_upload' => [
+            'driver' => 'local',
+            'root'   => public_path() . '/assets/cnet',
+        ],
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
