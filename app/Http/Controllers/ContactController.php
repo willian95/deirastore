fce3ae9470a7215f52a81b951de262003cf9c222
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-05-09 10:59:25 -0400

    cambios

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,19 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ContactStoreRequest;
 use App\Contact;
 
 class ContactController extends Controller
 {
     
-    function store(Request $request){
+    function store(ContactStoreRequest $request){
 
-        $contact = new Contact;
-        $contact->email = $request->email;
-        $contact->phone = $request->phone;
-        $contact->save();
+        try{
 
-        return response()->json(["msg" => "Excelente, te notificaremos cuando el sitio esté listo"]);
+            $contact = new Contact;
+            $contact->email = $request->email;
+            $contact->phone = $request->phone;
+            $contact->save();
+
+            return response()->json(["success" => true, "msg" => "Excelente, te notificaremos cuando el sitio esté listo"]);
+
+        }catch(\Exception $e){
+
+            return response()->json(["success" => false, "msg" => "Lo sentimos, estamos presentando problemas tecnicos"]);
+
+        }
 
     }
 
