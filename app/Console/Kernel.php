83d88c1eea31d5144eef084c0622f04b67ee6493
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-05-18 16:12:56 -0400

    cambios

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('products:nexsys')->everyMinute();
+        $schedule->command('products:nexsys')->dailyAt('01:00');;
     }
 
     /**
