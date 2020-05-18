9bb4c2c45b0c4e95d3a8376df4c56202958a74b1
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-05-18 16:11:31 -0400

    cambios

--- app/Console/Commands/NexsysProducts.php
+++ app/Console/Commands/NexsysProducts.php
@@ -44,115 +44,153 @@ class NexsysProducts extends Command
     {
         ini_set('max_execution_time', 0);
         //$params = ["encoding" => "UTF-8", "verifypeer" => false, "verifyhost" => false];
-        Log::info('Nexsys');
-        $mark = "ADATA";
+
         $url = "https://app.nexsysla.com/nexsysServiceSoap/NexsysServiceSoap?wsdl";
+        $marks = [
+            "3nStar",
+            "Adata",
+            "Adobe",
+            "AOC",
+            "APC",
+            "Arcserve",
+            "BIXOLON",
+            "Corel",
+            "Datalogic",
+            "Epson Escaner",
+            "Epson Gran Formato",
+            "Epson Impresión",
+            "Epson Pos",
+            "Epson Proyectores",
+            "HP Impresión Consumo",
+            "HP Impresión Corporativo",
+            "HP Pcs Empresariales",
+            "HP Portatiles Empresariales",
+            "HP POS",
+            "HP Workstation",
+            "Jabra",
+            "Kaspersky Consumo",
+            "Kaspersky Corporativo", 
+            "Lenovo Corporativo",
+            "Lenovo Servidores",
+            "Lenovo Workstation", 
+            "LG",
+            "Liferay",
+            "McAfee",
+            "OKI",
+            "Poly",
+            "RedHat",
+            "Sophos",
+            "Wacom",
+            "Xerox"
+        ];
 
-        try{
-        
-            $client = new \SoapClient($url);
-            $products = $client->StoreProductByMarks(["Marks" => $mark, "WSClient"=> ["country" => "Chile", "username" => "78198200"]]);
-            //dd($products);
-            $brandSlug = str_replace(" ", "-", $mark);
-            $brand = Brand::firstOrCreate(
-                ['name' => $mark],
-                ["slug" =>  $brandSlug]
-            );
+        foreach($marks as $mark){
+            Log::info($mark);
+            try{
+            
+                $client = new \SoapClient($url);
+                $products = $client->StoreProductByMarks(["Marks" => $mark, "WSClient"=> ["country" => "Chile", "username" => "78198200"]]);
+                //dd($products);
+                $brandSlug = str_replace(" ", "-", $mark);
+                $brand = Brand::firstOrCreate(
+                    ['name' => $mark],
+                    ["slug" =>  $brandSlug]
+                );
 
-            foreach($products as $product){
-                foreach($product as $value){
-                    
-                    if(is_string($value->category)){
+                foreach($products as $product){
+                    foreach($product as $value){
+                        
+                        if(is_string($value->category)){
 
-                        $fullCategory = substr($value->category, 3);
+                            $fullCategory = substr($value->category, 3);
 
-                        $categories = explode('>', $fullCategory);
+                            $categories = explode('>', $fullCategory);
 
-                        $index = 0;
-                        $model = null;
+                            $index = 0;
+                            $model = null;
 
-                        if(count($categories) > 2){
-                            array_pop($categories);
-                        }
+                            if(count($categories) > 2){
+                                array_pop($categories);
+                            }
 
-                        foreach($categories as $category){
-                          
-                            $string = trim($category);
-                            $slug = str_replace(" ", "-", $string);
-                            $slug = str_replace("/", "-", $slug);
+                            foreach($categories as $category){
+                            
+                                $string = trim($category);
+                                $slug = str_replace(" ", "-", $string);
+                                $slug = str_replace("/", "-", $slug);
 
-                            if($index == 1){
+                                if($index == 1){
 
-                                $previousSlug = str_replace(" ", "-", trim($categories[$index - 1]));
-                                $previousSlug = str_replace("/", "-", $previousSlug);
-                                
-                                
-                                $previousCategory = Category::where('slug', $previousSlug)->first();
-                                if(Category::where('slug', $slug)->where('parent_id', $previousCategory->id)->count() == 0){
-                                    $model = new Category;
-                                    $model->name = $string;
-                                    $model->slug = $slug;
-                                    $model->parent_id = $previousCategory->id;
-                                    $model->save();
+                                    $previousSlug = str_replace(" ", "-", trim($categories[$index - 1]));
+                                    $previousSlug = str_replace("/", "-", $previousSlug);
+                                    
+                                    
+                                    $previousCategory = Category::where('slug', $previousSlug)->first();
+                                    if(Category::where('slug', $slug)->where('parent_id', $previousCategory->id)->count() == 0){
+                                        $model = new Category;
+                                        $model->name = $string;
+                                        $model->slug = $slug;
+                                        $model->parent_id = $previousCategory->id;
+                                        $model->save();
+                                    }
+
+                                }else{
+
+                                    $model = Category::firstOrCreate(
+                                        ['name' => $string],
+                                        ['slug' => $slug]
+                                    );
+
                                 }
-
-                            }else{
-
-                                $model = Category::firstOrCreate(
-                                    ['name' => $string],
-                                    ['slug' => $slug]
-                                );
+                                
+                                $index++;
 
                             }
+
+                            $productSlug = str_replace(" ", "-", $value->short_description);
+                            $productSlug = str_replace("/", "-", $productSlug);
                             
-                            $index++;
+                            $excluded_tax = false;
+                            if($value->tax_excluded == "true")
+                                $excluded_tax = true;
+
+                            $amount = 0;
+                            if($value->inventory != "null")
+                                $amount = $value->inventory;
+
+                            $testProduct = Product::updateOrCreate(
+                                ["sku" => $value->sku],
+                                [
+                                    "category_id" => $model->id,
+                                    "brand_id" => $brand->id,
+                                    "currency" => $value->currency,
+                                    "picture" => $value->image,
+                                    "is_external" => true,
+                                    "parent_nexsys" => $value->parent,
+                                    "amount" => $amount,
+                                    "description" => $value->long_description,
+                                    "min_description" => $value->short_description,
+                                    "sub_title" => $value->short_description,
+                                    "price" => 0,
+                                    "data_source_id" => 1,
+                                    "tax_excluded" => $excluded_tax,
+                                    "name" => $value->short_description,
+                                    "slug" => $productSlug,
+                                    "external_price" => floatval($value->price)
+                                ]
+                            );
 
                         }
 
-                        $productSlug = str_replace(" ", "-", $value->short_description);
-                        $productSlug = str_replace("/", "-", $productSlug);
-                        
-                        $excluded_tax = false;
-                        if($value->tax_excluded == "true")
-                            $excluded_tax = true;
-
-                        $amount = 0;
-                        if($value->inventory != "null")
-                            $amount = $value->inventory;
-                        
-                        Log::info('Sku: '.$value->sku);
-
-                        $testProduct = Product::updateOrCreate(
-                            ["sku" => $value->sku],
-                            [
-                                "category_id" => $model->id,
-                                "brand_id" => $brand->id,
-                                "currency" => $value->currency,
-                                "picture" => $value->image,
-                                "is_external" => true,
-                                "parent_nexsys" => $value->parent,
-                                "amount" => $amount,
-                                "description" => $value->long_description,
-                                "min_description" => $value->short_description,
-                                "sub_title" => $value->short_description,
-                                "price" => 0,
-                                "data_source_id" => 1,
-                                "tax_excluded" => $excluded_tax,
-                                "name" => $value->short_description,
-                                "slug" => $productSlug,
-                                "external_price" => floatval($value->price)
-                            ]
-                        );
-
                     }
-
                 }
+
+                //return true;
+
+            }catch(\SoapFault $fault){
+                Log::info($fault);
             }
-
-            //return true;
-
-        }catch(\SoapFault $fault){
-            dd($fault);
         }
+    
     }
 }
