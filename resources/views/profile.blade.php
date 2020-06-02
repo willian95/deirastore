3b9c4bc43fd4758b54065508186cfb71212c3176
patriciax <yively.pa@gmail.com>
2020-06-02 12:33:09 -0400

    perfile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div class="container">
+    <div class="container bg card-form">
         <div class="row">
             <div class="col-12">
                 <a class="btn btn-primary btn-general btn-general--form" style="color: #fff;" href="{{ url()->previous() }}"><i class="fa fa-arrow-left"></i></a>
@@ -11,29 +11,52 @@
         <div class="row">
             <div class="col-lg-8 offset-lg-2 col-md-8 offset-md-2 col-12">
                 
-                <div class="card">
+                <div class="">
                     <div class="card-body">
-                        <h3 class="text-center">Perfil</h3>
-
-                        <div class="form-group">
-                            <label for="name">Nombre</label>
-                            <input type="text" class="form-control" id="name" aria-describedby="emailHelp" v-model="name">
+                        <div class="title__general fadeInUp wow animated">
+                            <p ></p><strong class="mr-5">Perfil</strong> </p>
                         </div>
 
                         <div class="form-group">
-                            <label for="rut">RUT</label>
-                            <input type="text" class="form-control" id="rut" aria-describedby="rut" v-model="rut">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label for="name">Nombre</label>
+                                    <input type="text" class="form-control" id="name" aria-describedby="emailHelp" v-model="name">
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="name">Apellido</label>
+                                    <input type="text" placeholder="Apellido" class="form-control" aria-describedby="emailHelp" v-model="lastname">
+                                </div>
+                            </div>
                         </div>
-
-                        <div class="form-group">
-                            <label for="name">Apellido</label>
-                            <input type="text" class="form-control" aria-describedby="emailHelp" v-model="lastname">
-                        </div>
-
                         <div class="form-group">
                             <label for="address">Dirección</label>
                             <input type="text" class="form-control" id="address" v-model="address">
                         </div>
+
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label for="rut">RUT</label>
+                                    <input type="text" class="form-control" id="rut" aria-describedby="rut" v-model="rut">
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="genre">Género</label>
+                                    <select class="form-control" id="genre" v-model="genre">
+                                        <option value="masculino">Masculino</option>
+                                        <option value="femenino">Femenino</option>
+                                        <option value="prefiero mantenerlo en privado">Prefiero mantenerlo en privado</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                      <!--  <div class="form-group">
+                            <label for="name">Apellido</label>
+                            <input type="text" class="form-control" aria-describedby="emailHelp" v-model="lastname">
+                        </div>
+
+                   
 
                         <div class="form-group">
                             <label for="genre">Género</label>
@@ -42,25 +65,39 @@
                                 <option value="femenino">Femenino</option>
                                 <option value="prefiero mantenerlo en privado">Prefiero mantenerlo en privado</option>
                             </select>
-                        </div>
+                        </div>-->
                         <div class="form-group">
-                            <label for="birthDate">Fec. Nacimiento</label>
-                            <input type="date" class="form-control" id="birthDate" aria-describedby="emailHelp" v-model="birthDate">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label for="birthDate">Fec. Nacimiento</label>
+                                    <input type="date" class="form-control" id="birthDate" aria-describedby="emailHelp" v-model="birthDate">
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="phoneNumber">Celular</label>
+                                        <input type="text" class="form-control" id="phoneNumber" aria-describedby="emailHelp" v-model="phoneNumber" @keypress="isTelephoneNumber($event)">
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                       
                         <div class="form-group">
-                            <label for="phoneNumber">Celular</label>
-                            <input type="text" class="form-control" id="phoneNumber" aria-describedby="emailHelp" v-model="phoneNumber" @keypress="isTelephoneNumber($event)">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label for="password">Clave</label>
+                                    <input type="password" class="form-control" id="password" v-model="password">
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="passwordRepeat">Repetir Clave</label>
+                                        <input type="password" class="form-control" id="passwordRepeat" v-model="passwordRepeat">
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="password">Clave</label>
-                            <input type="password" class="form-control" id="password" v-model="password">
-                        </div>
-                        <div class="form-group">
-                            <label for="passwordRepeat">Repetir Clave</label>
-                            <input type="password" class="form-control" id="passwordRepeat" v-model="passwordRepeat">
-                        </div>
-                        <div class="form-group">
-                            <button class="btn btn-primary" @click="update()">Actualizar</button>
+                     
+                        <div class="form-group text-center">
+                            <button class="btn btn-primary btn-general" @click="update()">Actualizar</button>
                         </div>
 
                     </div>
