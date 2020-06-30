33a722398433d18122d904df47b0e62bbcceae84
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-06-30 17:54:52 -0400

    cambios

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -136,11 +136,17 @@ class CheckoutController extends Controller
 				$productPurchase->product_id = $cart["id"];
 				$productPurchase->amount = $cart["amount"];
 				
-				if($product->external_price > 0 && $product->price == 0){ //si el producto cuenta con precio externo mayor a 0 y precio = 0
-					$productPurchase->price = intval(($product->external_price * DolarPrice::first()->price) + 1); //multiplica el precio en USD por el valor en CLP
-				}else{	
-					$productPurchase->price = $product->price * $cart["amount"];
+				if($product->product->percentage_range_profit != 0 && $product->product->percentage_range_profit != null){
+					$productPurchase->price = intval($product->product->price_range_profit * DolarPrice::first()->price) + 1;
+				}else{
+					$productPurchase->price = intval($product->product->external_price * App\DolarPrice::first()->price) + 1;
 				}
+
+				/*if($product->external_price > 0 && $product->price_range_profit == 0){ //si el producto cuenta con precio externo mayor a 0 y precio = 0
+					$productPurchase->price = intval(($product->external_price * DolarPrice::first()->price) + 1); //multiplica el precio en USD por el valor en CLP
+				}else if($product->external_price > 0 && $product->price_range_profit == 0){	
+					$productPurchase->price = intval(($product->price_range_profit * DolarPrice::first()->price) + 1);
+				}*/
 
 				$productPurchase->save();
 
