ca366d4e04bd37a9fa908af3cc3dbd94b50d0114
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-06-30 21:45:36 -0400

    cambios

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -11,13 +11,13 @@ class FileController extends Controller
 {
     function update(){
 
-        ini_set('max_execution_time', 0);
+        /*ini_set('max_execution_time', 0);
         $connection = ssh2_connect('200.27.164.195', 22);
         ssh2_auth_password($connection, 'root', 'Terminal*1');
 
         ssh2_scp_recv($connection, '/home/ftpingram/CLPriceFileDeira.csv.zip', public_path('/')."/CLPriceFileDeira.csv.zip");
-        ob_end_clean();
-        system('unzip CLPriceFileDeira.csv.zip');
+        ob_end_clean();*/
+        //system('unzip CLPriceFileDeira.csv.zip');
 
         Excel::import(new IngramImport, 'CLPriceFileDeira.csv');
         /*$unzip = new ZipArchive;
