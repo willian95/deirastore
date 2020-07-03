2e4680311542b7b5ad89112b3af224f8a9695ef5
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-07-02 23:51:38 -0400

    cambios

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -14,8 +14,9 @@ class FileController extends Controller
         ini_set('max_execution_time', 0);
         $connection = ssh2_connect('200.27.164.195', 22);
         ssh2_auth_password($connection, 'root', 'Terminal*1');
-
-        ssh2_scp_recv($connection, '/home/ftpingram/CLPriceFileDeira.csv.zip', public_path('/')."CLPriceFileDeira.csv.zip");
+        $stream = ssh2_exec($connection, 'unzip /home/ftpingram/CLPriceFileDeira.csv.zip');
+        //ssh2_scp_recv($connection, '/home/ftpingram/CLPriceFileDeira.csv.zip', public_path('/')."CLPriceFileDeira.csv.zip");
+        dd("done");
         ob_end_clean();
         system('unzip CLPriceFileDeira.csv.zip');
 
