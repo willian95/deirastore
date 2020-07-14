732b49a282eb3b6069f5f9eaa916f1f7a28c709e
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-07-14 18:56:55 -0400

    cambios

--- resources/views/admin/sale.blade.php
+++ resources/views/admin/sale.blade.php
@@ -120,6 +120,26 @@
                                 <p><strong>Dept / Casa /oficina</strong></p>
                                 <p>@{{ saleDetails.user.house }}</p>
                             </div>
+                            <div class="col-4" v-if="saleDetails.user && saleDetails.tickt_type == 'factura'">
+                                <p><strong>Razón social empresa</strong></p>
+                                <p>@{{ saleDetails.user.business_name }}</p>
+                            </div>
+                            <div class="col-4" v-if="saleDetails.user && saleDetails.tickt_type == 'factura'">
+                                <p><strong>RUT empresa</strong></p>
+                                <p>@{{ saleDetails.user.business_rut }}</p>
+                            </div>
+                            <div class="col-4" v-if="saleDetails.user && saleDetails.tickt_type == 'factura'">
+                                <p><strong>Dirección empresa</strong></p>
+                                <p>@{{ saleDetails.user.business_address }}</p>
+                            </div>
+                            <div class="col-4" v-if="saleDetails.user && saleDetails.tickt_type == 'factura'">
+                                <p><strong>Teléfono empresa</strong></p>
+                                <p>@{{ saleDetails.user.business_phone }}</p>
+                            </div>
+                            <div class="col-4" v-if="saleDetails.user && saleDetails.tickt_type == 'factura'">
+                                <p><strong>Correo empresa</strong></p>
+                                <p>@{{ saleDetails.user.business_mail }}</p>
+                            </div>
                             <div class="col-4" v-if="saleDetails.guest">
                                 <p><strong>Nombre</strong></p>
                                 <p>@{{ saleDetails.guest.name }}</p>
