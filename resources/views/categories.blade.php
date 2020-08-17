3da78a86eee9211ca9034547e6fc85fed17b657a
patriciax <yively.pa@gmail.com>
2020-08-17 13:15:48 -0400

    menu - header

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -2,91 +2,93 @@
 
 @section('content')
 
-    <div class="container bg" id="dev-categories">
+<div class="container bg" id="dev-categories">
 
-        <div class="row" v-cloak>
-           <!--- <div class="col-12">
+    <div class="row" v-cloak>
+        <!--- <div class="col-12">
                 <a class="btn btn-primary btn-general btn-general--form" style="color: #fff;" href="{{ url()->previous() }}"><i class="fa fa-arrow-left"></i></a>
             </div>--->
-        </div>
-        <div class="title__general fadeInUp wow animated">
-            <p><strong>Todas las </strong>Categorias</p>
-        </div>
-        <div class="" v-cloak>
-            <ul class="categories__grid">
-                <div v-for="category in categories">
+    </div>
+    <div class="title__general fadeInUp wow animated">
+        <p><strong>Todas las </strong>Categorias</p>
+    </div>
 
-                    <li class="nav-item" v-if="category.child.length == 0 && category.parent_id == null">
-                        <a class="nav-link" :href='"{{ url("/category/") }}"+"/"+category.slug' v-if="category.esp_name">@{{ category.esp_name  }}</a>
-                        <!--<a class="nav-link" :href='"{{ url("/category/") }}"+"/"+category.slug' v-else="category.name">@{{ category.name  }}</a>-->
-                    </li>
 
-                    <li class="nav-item dropdown mega-menu" v-if="category.child.length > 0">
-                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">@{{ category.esp_name }}</a>
-                        <div class="dropdown-menu" style="opacity: 1;">
-                            <div class="grid-menu">
-                                <div class="grid-menu__item">
-                                    <ul v-if="category.child.length > 0">
-                                        <li v-for="child in category.child">
-                                            <a class="dropdown-item" :href='"{{ url("/category/") }}"+"/"+child.slug'>@{{ child.esp_name }}</a>
-                                        </li>                         
-                                    </ul>
-                                </div>
+    <div class="" v-cloak>
+        <ul class="categories__grid">
+            <div v-for="category in categories">
+
+                <li class="nav-item dropdown-toggle" v-if="category.child.length == 0 && category.parent_id == null">
+                    <a class="nav-link" :href='"{{ url("/category/") }}"+"/"+category.slug' v-if="category.esp_name">@{{ category.esp_name  }}</a>
+                    <!--<a class="nav-link" :href='"{{ url("/category/") }}"+"/"+category.slug' v-else="category.name">@{{ category.name  }}</a>-->
+                </li>
+
+                <li class="nav-item dropdown mega-menu" v-if="category.child.length > 0">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">@{{ category.esp_name }}</a>
+                    <div class="dropdown-menu" style="opacity: 1;">
+                        <div class="grid-menu">
+                            <div class="grid-menu__item">
+                                <ul v-if="category.child.length > 0">
+                                    <li v-for="child in category.child">
+                                        <a class="dropdown-item" :href='"{{ url("/category/") }}"+"/"+child.slug'>@{{ child.esp_name }}</a>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
-                    </li>
+                    </div>
+                </li>
 
-                </div>
-                
-            </ul>
+            </div>
 
-            <button @click="moreItems()" v-if="page < maxPages && loading == false" class="btn btn-primary btn-general btn-general--form" style="color: #fff; height: 60px; width: 120px;">cargar más</button>
-          
-        </div>
+        </ul>
 
-
-
+        <button @click="moreItems()" v-if="page < maxPages && loading == false" class="btn btn-primary btn-general btn-general--form" style="color: #fff; height: 60px; width: 120px;">cargar más</button>
 
     </div>
-    @include('partials.footer')
+
+
+
+
+</div>
+@include('partials.footer')
 
 @endsection
 
 @push('scripts')
 
-    <script>
-        const navbar = new Vue({
-            el: '#dev-categories',
-            data(){
-                return{
-                    categories:null,
-                    page:1,
-                    maxPages:0,
-                    loading:false
-                }
-            },
-            methods:{
-                
-                getItems(){
-                   
-                    this.loading = true
-                    axios.get("{{ url('/categories/menu') }}"+"/"+this.page)
+<script>
+    const navbar = new Vue({
+        el: '#dev-categories',
+        data() {
+            return {
+                categories: null,
+                page: 1,
+                maxPages: 0,
+                loading: false
+            }
+        },
+        methods: {
+
+            getItems() {
+
+                this.loading = true
+                axios.get("{{ url('/categories/menu') }}" + "/" + this.page)
                     .then(res => {
                         //console.log(res)
                         this.loading = false
-                        if(res.data.success == true){
-                            if(this.categories == null){
+                        if (res.data.success == true) {
+                            if (this.categories == null) {
                                 this.categories = res.data.categories
-                                
-                            }else{
+
+                            } else {
                                 res.data.categories.forEach((data, index) => {
                                     this.categories.push(data)
                                 })
                             }
 
-                            this.maxPages = Math.ceil(res.data.categoriesCount/25)
+                            this.maxPages = Math.ceil(res.data.categoriesCount / 25)
 
-                        }else{
+                        } else {
 
                             alertify.error(res.data.msg)
 
@@ -98,20 +100,20 @@
                         alertify.error("Error en el servidor")
                         //console.log(err.response.data)
                     })
-                },
-                moreItems(){
-                    this.page ++;
-                    this.getItems()
-                }
-
             },
-            mounted(){
-                
+            moreItems() {
+                this.page++;
                 this.getItems()
-
             }
 
-        })
-    </script>
+        },
+        mounted() {
+
+            this.getItems()
+
+        }
+
+    })
+</script>
 
 @endpush
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,119 +1,121 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title>Deira store | Productos Informáticos \</title>  
-        <meta name="description" content= "Compra online tecnología, informática ,Amplia variedad de productos descubre las mejores marcas del mercado tecnológico" />
-        <meta content='Computación, ventas, ecommerce,informatica,DeiraTic,Deira, DeiraStore,DeiraStore Venta de Productos Informáticos ,Santiago, Chile ' property='og:description'>
-        <meta name="robots" content="index, follow">
-        <meta name="dc.language" content="es">
-        <meta name="dc.source" content="url del sitio">
-        <meta itemprop="url" content="url temporal del sitio">
-        <meta name="keywords" content="informática ,usb,disco duro,Productos Informáticos, memorias ram, ADATA , 3nstar, Aoc , Sophos,ram ddr2,comprar memoria ram, disco duro ">
-        <meta content='iso' property='og:image'>
-        <meta property="og:locale" content="es_ES" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Deira" />
-        <meta property="og:description" content="La mejor tienda de productos electrónicos" />
-        <meta http-equiv="cache-control" content="no-cache"/>
-         <!---links----->
-        <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
-        <link href="{{ asset('assets/css/slick.css') }}" rel="stylesheet" />
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css" rel="stylesheet" />
-        <link href="{{ asset('assets/css/slick-theme.css') }}" rel="stylesheet" />
-        <link href="{{ asset('css/font-awesome.css') }}" rel="stylesheet" />
-     
-        <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet" />
-        <link href="{{ asset('assets/css/admin.css') }}" rel="stylesheet" />
-        <link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet" />
-        <link href="{{ asset('alertify/css/alertify.min.css') }}" rel="stylesheet" />
-        <link href="{{ asset('alertify/css/themes/bootstrap.min.css') }}" rel="stylesheet" />
-        {!! ReCaptcha::htmlScriptTagJsApi() !!}
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Deira store | Productos Informáticos \</title>
+    <meta name="description" content="Compra online tecnología, informática ,Amplia variedad de productos descubre las mejores marcas del mercado tecnológico" />
+    <meta content='Computación, ventas, ecommerce,informatica,DeiraTic,Deira, DeiraStore,DeiraStore Venta de Productos Informáticos ,Santiago, Chile ' property='og:description'>
+    <meta name="robots" content="index, follow">
+    <meta name="dc.language" content="es">
+    <meta name="dc.source" content="url del sitio">
+    <meta itemprop="url" content="url temporal del sitio">
+    <meta name="keywords" content="informática ,usb,disco duro,Productos Informáticos, memorias ram, ADATA , 3nstar, Aoc , Sophos,ram ddr2,comprar memoria ram, disco duro ">
+    <meta content='iso' property='og:image'>
+    <meta property="og:locale" content="es_ES" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Deira" />
+    <meta property="og:description" content="La mejor tienda de productos electrónicos" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <!---links----->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/slick.css') }}" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css" rel="stylesheet" />
+    <link href="{{ asset('assets/css/slick-theme.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/font-awesome.css') }}" rel="stylesheet" />
 
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-173437279-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/admin.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet" />
+    <link href="{{ asset('alertify/css/alertify.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('alertify/css/themes/bootstrap.min.css') }}" rel="stylesheet" />
+    {!! ReCaptcha::htmlScriptTagJsApi() !!}
 
-  gtag('config', 'UA-173437279-1');
-</script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-173437279-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-    </head>
-    <body>
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
 
-        @include('partials.navbar')
-        <div id="dev-app">
-            @yield('content')
-        </div>
-        
-        <script src="{{ asset('assets/js/jquery.min.js ') }}"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/blazy/1.8.2/blazy.min.js"></script>
-        <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-        <script src="{{ asset('assets/js/slick.min.js') }}"></script>
-        <script src="{{ asset('assets/js/setting-slick.js') }}"></script>
-        <script src="{{ asset('assets/js/main.js') }}"></script>
-        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-        <script src="{{ asset('alertify/alertify.min.js') }}"></script>
-        <script src="{{ asset('/js/app.js') }}"></script>
+        gtag('config', 'UA-173437279-1');
+    </script>
 
-        <script>
+</head>
 
-            const search = new Vue({
-                el: '#search-area',
-                data(){
-                    return{
-                        searchText:""
-                    }
-                },
-                methods:{
-                    
-                    search(){
-                        
-                        if(this.searchText != ""){
-                            localStorage.setItem("search", this.searchText)
-                            window.location.href="{{ url('/search') }}"
-                        }
+<body>
 
+    @include('partials.navbar')
+    <div id="dev-app">
+        @yield('content')
+    </div>
+
+    <script src="{{ asset('assets/js/jquery.min.js ') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/blazy/1.8.2/blazy.min.js"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/slick.min.js') }}"></script>
+    <script src="{{ asset('assets/js/setting-slick.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="{{ asset('alertify/alertify.min.js') }}"></script>
+    <script src="{{ asset('/js/app.js') }}"></script>
+
+    <script>
+        const search = new Vue({
+            el: '#search-area',
+            data() {
+                return {
+                    searchText: ""
+                }
+            },
+            methods: {
+
+                search() {
+
+                    if (this.searchText != "") {
+                        localStorage.setItem("search", this.searchText)
+                        window.location.href = "{{ url('/search') }}"
                     }
 
                 }
 
-            }) 
+            }
 
-            const searchMobile = new Vue({
-                el: '#search-area-mobile',
-                data(){
-                    return{
-                        searchText:""
-                    }
-                },
-                methods:{
-                    
-                    search(){
-                        
-                        if(this.searchText != ""){
-                            localStorage.setItem("search", this.searchText)
-                            window.location.href="{{ url('/search') }}"
-                        }
+        })
 
+        const searchMobile = new Vue({
+            el: '#search-area-mobile',
+            data() {
+                return {
+                    searchText: ""
+                }
+            },
+            methods: {
+
+                search() {
+
+                    if (this.searchText != "") {
+                        localStorage.setItem("search", this.searchText)
+                        window.location.href = "{{ url('/search') }}"
                     }
 
                 }
 
-            }) 
+            }
 
-        </script>
+        })
+    </script>
 
-        <script>
-            alertify.set('notifier','position', 'top-right');
-            (function($){
-                $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+    <script>
+        alertify.set('notifier', 'position', 'top-right');
+        (function($) {
+            $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
                 if (!$(this).next().hasClass('show')) {
                     $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
                 }
@@ -125,42 +127,41 @@
                 });
 
                 return false;
-                });
-            })(jQuery)
-        </script>
-
-        <script>
-            new WOW().init();
-
-
-            $(document).ready(function() {
-                $(".megamenu").on("click", function(e) {
-                    e.stopPropagation();
-                });
-
-                $("#menu-categories").on("click", function(){
-                    if($("#menu-categories-dropdown").hasClass('show')){
-                        $("#menu-categories-dropdown").removeClass('show')
-                    }else{
-                        $("#menu-categories-dropdown").addClass('show')
-                    }
-                })
-
-                $("#menu-brands").on("click", function(){
-                    if($("#menu-brands-dropdown").hasClass('show')){
-                        $("#menu-brands-dropdown").removeClass('show')
-                    }else{
-                        $("#menu-brands-dropdown").addClass('show')
-                    }
-                })
-
             });
-            
-        </script>
-        
-        @stack('scripts')
-    </body>
+        })(jQuery)
+    </script>
 
-    
+    <script>
+        new WOW().init();
+
+
+        $(document).ready(function() {
+            $(".megamenu").on("click", function(e) {
+                e.stopPropagation();
+            });
+
+            $("#menu-categories").on("click", function() {
+                if ($("#menu-categories-dropdown").hasClass('show')) {
+                    $("#menu-categories-dropdown").removeClass('show')
+                } else {
+                    $("#menu-categories-dropdown").addClass('show')
+                }
+            })
+
+            $("#menu-brands").on("click", function() {
+                if ($("#menu-brands-dropdown").hasClass('show')) {
+                    $("#menu-brands-dropdown").removeClass('show')
+                } else {
+                    $("#menu-brands-dropdown").addClass('show')
+                }
+            })
+
+        });
+    </script>
+
+    @stack('scripts')
+</body>
+
+
 
 </html>
