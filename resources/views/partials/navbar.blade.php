3cfcca0a444982b5274df12712f2387638005299
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-08-25 13:05:28 -0400

    cambios

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -31,7 +31,7 @@
                         @endif
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                             @if(Auth::check() && Auth::user()->id)
-                                @if(\Auth::check() && Auth::user()->role_id == 3)
+                                @if(\Auth::check() && Auth::user()->rol_id == 3)
                                     <a href="{{ url('/admin/dashboard') }}" class="drow-none">Administrador</a>
                                 @endif
                                 <a href="{{ url('/profile') }}" class="drow-none">Mis Datos</a>
