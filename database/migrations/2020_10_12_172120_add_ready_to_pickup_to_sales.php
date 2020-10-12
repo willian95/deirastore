743cdeb728250db5185b65e6fa16a543c20797f3
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-10-12 13:22:28 -0400

    cambios

---
+++ database/migrations/2020_10_12_172120_add_ready_to_pickup_to_sales.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddReadyToPickupToSales extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->boolean("ready_to_pickup")->default(false);
+            $table->boolean("ready_to_ship")->default(false);
+            $table->string("tracking")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pickup_to_sales', function (Blueprint $table) {
+            //
+        });
+    }
+}
